af373db0d13bead0c5b6dc8f817859d9c751ccb6
Nicolas Aspert <nicolas.aspert@epfl.ch>
2018-08-24 17:23:40 +0200

    begin parsing file

--- src/main/scala/wiki/PageParse.scala
+++ src/main/scala/wiki/PageParse.scala
@@ -13,12 +13,29 @@ import org.apache.spark.sql.{SQLContext, Row, DataFrame}
 import scala.RuntimeException
 
 object PageParse {
+  
+  def parseLine(line: String):List[String] = {
+    val grp = """\((.*?)\)""".r
+    val spl = line.split(" VALUES ")(1)
+    grp.findAllIn(spl).toList
+    
+  }
+    
+  
   def main(args: Array[String]) {
-    var pageFilePath = args(0)
-    println("Using %s".format(pageFilePath))
+    var filePath = args(0)
+    var fileType = args(1)
+    println("Reading %s".format(filePath))
+    
     val conf = new SparkConf().setAppName("Wikipedia dump parser").setMaster("local[2]")
     val sc = new SparkContext(conf)
-    val file = sc.textFile(pageFilePath, 4)
+    
+    val lines = sc.textFile(filePath, 4)
+    val sqlLines = lines.filter(l => l.startsWith("INSERT INTO `%s` VALUES".format(fileType)))
+    val records = sqlLines.map(l => parseLine(l))
+    val rec_len = records.map(_.length)
+    println("File contains %d lines, %d inserts and %d records".format(lines.count(), sqlLines.count(), rec_len.sum().toInt))
+    
   }
 
 }
