0d35381525aa8fe92c4b3cafff6a2af4b340ada6
Nicolas Aspert <nicolas.aspert@epfl.ch>
2018-08-31 15:19:55 +0200

    allow DumpParseMerge to process redirects

--- src/main/scala/wiki/DumpParseMerge.scala
+++ src/main/scala/wiki/DumpParseMerge.scala
@@ -13,13 +13,38 @@ import scala.RuntimeException
 import org.rogach.scallop._
 
 class ConfMerge(args: Seq[String]) extends ScallopConf(args) {
-  val pagePath = opt[String](required = true, name= "pagePath")
-  val pageLinksPath = opt[String](required = true, name="pageLinksPath")
+  val pagePath = opt[String](required = true, name="pagePath")
+  val pageLinksPath = opt[String](name="pageLinksPath")
+  val redirectPath = opt[String](name="redirectPath")
   val outputPath = opt[String](required = true, name="outputPath")
+  requireOne(pageLinksPath, redirectPath)
   verify()
 }
 
 object DumpParseMerge {
+  def joinPageLinks(session:SparkSession, pages:DataFrame, pageLinkPath:String, outputPath:String) = {
+    val pagelinks = session.read.parquet(pageLinkPath)
+    val pagelinks_id = pagelinks.join(pages, "title").select("from", "id", "title")
+    
+    pagelinks_id.write.option("delimiter", "\t")
+            .option("header", false)
+            .option("quote", "")
+            .option("compression", "gzip")
+            .csv(outputPath)
+  }
+  
+  def joinRedirect(session:SparkSession, pages:DataFrame, redirectPath:String, outputPath:String) = {
+    val redirect = session.read.parquet(redirectPath)
+    val redirect_id = redirect.join(pages, "title").select("from", "id", "title")
+    
+    redirect_id.write.option("delimiter", "\t")
+            .option("header", false)
+            .option("quote", "")
+            .option("compression", "gzip")
+            .csv(outputPath)
+  }
+  
+  
   def main(args: Array[String]) {
     val conf = new ConfMerge(args)
     println("Reading %s and %s".format(conf.pagePath(), conf.pageLinksPath()))
@@ -27,12 +52,11 @@ object DumpParseMerge {
     val session = SparkSession.builder.config(sconf).getOrCreate()
     val sctx = session.sparkContext
     val pages = session.read.parquet(conf.pagePath())
-    val pagelinks = session.read.parquet(conf.pageLinksPath())
-    val pagelinks_id = pagelinks.join(pages, "title").select("from", "id", "title")
-    pagelinks_id.write.option("delimiter", "\t")
-            .option("header", false)
-            .option("quote", "")
-            .option("compression", "gzip")
-            .csv(conf.outputPath())
+    conf.pageLinksPath.toOption match {
+      case None => joinRedirect(session, pages, conf.redirectPath(), conf.outputPath())
+      case _ => joinPageLinks(session, pages, conf.pageLinksPath(), conf.outputPath())
+    }
+    
+   
   }
 }
