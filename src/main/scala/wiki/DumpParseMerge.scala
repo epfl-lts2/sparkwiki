25242b36bf851bac613a69ce735608f0317f208a
Nicolas Aspert <nicolas.aspert@epfl.ch>
2018-09-05 14:44:27 +0200

    simplify page/category processing

--- src/main/scala/wiki/DumpParseMerge.scala
+++ src/main/scala/wiki/DumpParseMerge.scala
@@ -41,32 +41,12 @@ object DumpParseMerge {
   }
   def joinPageLinks(session:SparkSession, pages:DataFrame, pageLinkPath:String, outputPath:String) = {
     import session.implicits._
-    val normal_pages = pages.filter($"namespace" === 0).select("id", "title")
-    val cat_pages = pages.filter($"namespace" === 14).select("id", "title")
-    
+   
     val pagelinks = session.read.parquet(pageLinkPath)
-    
-    // create normal page links (relation LINKS_TO)
-    val normal_links = pagelinks.filter($"fromNamespace" === 0 && $"namespace" === 0)
-                                .join(normal_pages, "title")
+    val pagelinks_id = pagelinks.join(pages, Seq("title", "namespace"))
                                 .select("from", "id", "title", "fromNamespace", "namespace")
-    writeCsv(normal_links, Paths.get(outputPath, "normal_links").toString)
-
-    val cat_links = pagelinks.filter($"fromNamespace" === 14 && $"namespace" === 14)
-                             .join(cat_pages, "title")
-                             .select("from", "id", "title", "fromNamespace", "namespace")
-    writeCsv(cat_links, Paths.get(outputPath, "cat_links").toString)
-    
-    val n2c_links = pagelinks.filter($"fromNamespace" === 0 && $"namespace" === 14)
-                             .join(cat_pages, "title")
-                             .select("from", "id", "title", "fromNamespace", "namespace")
-    writeCsv(n2c_links, Paths.get(outputPath, "n2c_links").toString)
-    
-    val c2n_links = pagelinks.filter($"fromNamespace" === 14 && $"namespace" === 0)
-                             .join(normal_pages, "title")
-                             .select("from", "id", "title", "fromNamespace", "namespace")
-    writeCsv(c2n_links, Paths.get(outputPath, "c2n_links").toString)
-  }
+    writeCsv(pagelinks_id, outputPath)                            
+   }
   
   def joinRedirect(session:SparkSession, pages:DataFrame, redirectPath:String, outputPath:String) = {
     val redirect = session.read.parquet(redirectPath)
@@ -98,22 +78,10 @@ object DumpParseMerge {
     
     val pages = session.read.parquet(conf.pagePath())
     
-    if (conf.pageLinksPath.isEmpty && conf.catlinksPath.isEmpty && conf.redirectPath.isEmpty) {
-        splitPages(session, pages, conf.outputPath())
-    } else {
-    
-      conf.catlinksPath.toOption match {
-        case None => {
-          conf.pageLinksPath.toOption match {
-            case None => joinRedirect(session, pages, conf.redirectPath(), conf.outputPath())
-            case _ => joinPageLinks(session, pages, conf.pageLinksPath(), conf.outputPath())
-          }
-        }
-        case _ => {
-          joinCategory(session, pages, conf.catlinksPath(), conf.outputPath())
-        }
-      }    
-    }
-   
+    if (!conf.pageLinksPath.isEmpty) joinPageLinks(session, pages, conf.pageLinksPath(), conf.outputPath())
+    else if (!conf.catlinksPath.isEmpty) joinCategory(session, pages, conf.catlinksPath(), conf.outputPath())
+    else if (!conf.redirectPath.isEmpty) joinRedirect(session, pages, conf.redirectPath(), conf.outputPath())
+    else splitPages(session, pages, conf.outputPath())
+      
   }
 }
--- src/main/scala/wiki/WikipediaElement.scala
+++ src/main/scala/wiki/WikipediaElement.scala
@@ -16,7 +16,7 @@ trait WikipediaElementParser[T <: WikipediaElement with Product] {
 
 case class WikipediaPage(id:Int, namespace:Int, title:String, restriction:String, counter:Int, 
                           isRedirect:Boolean, isNew:Boolean, random:Double, touched:Timestamp, linksUpdated:String,
-                          latest:Int, len:Int, contentModel:String, lang:String) extends WikipediaElement 
+                          latest:Int, len:Int, contentModel:String, lang:String, isCategory:Boolean) extends WikipediaElement 
                           
 case class WikipediaPageLink(from:Int, namespace:Int, title:String, fromNamespace:Int) extends WikipediaElement 
 
@@ -54,7 +54,7 @@ class WikipediaPageParser extends Serializable with WikipediaElementParser[Wikip
     r.map(m =>  WikipediaPage(m.group(1).toInt, m.group(2).toInt, m.group(3), m.group(4), m.group(5).toInt,
                         m.group(6).toInt == 1, m.group(7).toInt == 1, m.group(8).toDouble, 
                         new Timestamp(timestampFormat.parse(m.group(9)).getTime), m.group(10), m.group(11).toInt, 
-                        m.group(12).toInt, m.group(13), m.group(14)))
+                        m.group(12).toInt, m.group(13), m.group(14), m.group(2).toInt == 14))
       
   }
     
@@ -62,7 +62,7 @@ class WikipediaPageParser extends Serializable with WikipediaElementParser[Wikip
   def filterElt(t: WikipediaPage):Boolean = (t.namespace == 0 || t.namespace == 14) && t.id > 0
   def getDataFrame(session:SparkSession, lines: RDD[String]):DataFrame = {
     session.createDataFrame(lines.flatMap(l => parseLine(l)).filter(filterElt))
-                   .select("id", "namespace", "title", "isRedirect", "isNew")
+                   .select("id", "namespace", "title", "isRedirect", "isNew", "isCategory")
   }
 }
 
