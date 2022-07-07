c7c5b3f9784babb272706d3ebc899c13b8043c4a
Nicolas Aspert <nicolas.aspert@epfl.ch>
2022-07-07 08:20:14 +0200

    fix test run on macos without connection

--- src/test/scala/ch/epfl/lts2/wikipedia/SparkSessionTestWrapper.scala
+++ src/test/scala/ch/epfl/lts2/wikipedia/SparkSessionTestWrapper.scala
@@ -1,8 +1,13 @@
 package ch.epfl.lts2.wikipedia
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
 
 trait SparkSessionTestWrapper {
+  val conf = new SparkConf()
+    .setAppName("spark test runner")
+    .setMaster("local")
+    .set("spark.driver.host", "localhost")
   lazy val spark: SparkSession = {
-    SparkSession.builder().master("local").appName("spark test runner").getOrCreate()
+    SparkSession.builder().config(conf).getOrCreate()
   }
 }
